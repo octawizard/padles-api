eb825757b5e24c4e26fb21dc13b8c8550e4281d4
roberto.manca <roberto.manca@schibsted.com>
2020-10-07 16:31:13 +0200

    add utils test for retry function

---
+++ src/test/kotlin/com/octawizard/repository/utilsTest.kt
@@ -0,0 +1,43 @@
+package com.octawizard.repository
+
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class UtilsTest {
+
+    @Test
+    fun `retry function should retry executing block default times at most when block throws exceptions`() {
+        val block = mockk<suspend () -> Unit>()
+        coEvery { block() } throws RuntimeException("test retry")
+
+        runBlocking { retry { block() } }
+
+        coVerify(exactly = 3) { block() }
+    }
+
+    @Test
+    fun `retry function should retry executing block given times at most when block throws exceptions`() {
+        val block = mockk<suspend () -> Unit>()
+        coEvery { block() } throws RuntimeException("test retry")
+
+        runBlocking { retry(1) { block() } }
+
+        coVerify(exactly = 1) { block() }
+    }
+
+    @Test
+    fun `retry function should retry executing block until block invocation isn't throwing exception`() {
+        val block = mockk<suspend () -> Unit>()
+        coEvery { block() } throws RuntimeException("test retry") andThen Unit
+
+        runBlocking { retry(5) { block() } }
+
+        coVerify(exactly = 2) { block() }
+    }
+
+}
