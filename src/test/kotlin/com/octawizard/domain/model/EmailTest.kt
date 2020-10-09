c5b0c47a945b9607e0c295252e60d703ffc54575
roberto.manca <roberto.manca@schibsted.com>
2020-10-09 12:35:16 +0200

    Add Email test

---
+++ src/test/kotlin/com/octawizard/domain/model/EmailTest.kt
@@ -0,0 +1,34 @@
+package com.octawizard.domain.model
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import java.lang.IllegalArgumentException
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class EmailTest {
+
+    @Test
+    fun `Email should validate its value`() {
+        val value = "valid@test.com"
+        val email = Email(value)
+        assertEquals(value, email.value)
+    }
+
+    @Test
+    fun `Email should not validate empty value`() {
+        assertThrows(IllegalArgumentException::class.java) { Email("") }
+    }
+
+    @Test
+    fun `Email should not validate invalid value`() {
+        assertThrows(IllegalArgumentException::class.java) { Email("not an email") }
+    }
+
+    @Test
+    fun `Email should not be equals to another object`() {
+        assertFalse(Email("test@test.com").equals(1))
+    }
+}
