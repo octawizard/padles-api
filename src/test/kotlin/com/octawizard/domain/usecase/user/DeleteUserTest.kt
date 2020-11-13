70448b1511516d7c40b0992592f07c895b76215e
roberto.manca <roberto.manca@schibsted.com>
2020-11-13 08:50:50 +0100

    Fix test for delete user usecase

--- src/test/kotlin/com/octawizard/domain/usecase/user/DeleteUserTest.kt
+++ src/test/kotlin/com/octawizard/domain/usecase/user/DeleteUserTest.kt
@@ -1,12 +1,9 @@
 package com.octawizard.domain.usecase.user
 
 import com.octawizard.domain.model.Email
-import com.octawizard.domain.model.User
 import com.octawizard.repository.user.UserRepository
-import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 
@@ -16,13 +13,10 @@ class DeleteUserTest {
     @Test
     fun `DeleteUser call repository to delete an user`() {
         val email = Email("test@email.com")
-        val user = User(email, "test")
         val repository = mockk<UserRepository>(relaxed = true)
         val deleteUser = DeleteUser(repository)
 
-        every { repository.deleteUser(email) }
-
-        assertEquals(user, deleteUser.invoke(email))
+        deleteUser.invoke(email)
         verify(exactly = 1) { repository.deleteUser(email) }
     }
 }
